3248055932b1f51755c30b10a4da15dc888b1394
Vinicius Dias <carlosv775@gmail.com>
2020-04-27 19:33:11 -0300

    DI configurado com eventos ainda embutidos na config de DI

--- src/Academico/Apresentacao/CLI/matricula-de-aluno.php
+++ src/Academico/Apresentacao/CLI/matricula-de-aluno.php
@@ -2,29 +2,13 @@
 
 require_once 'vendor/autoload.php';
 
-use Alura\Arquitetura\Academico\Dominio\Aluno\AlunoMatriculado;
-use Alura\Arquitetura\Shared\Infra\Evento\LeagueEvent\EmitidorEvento as LeagueEventEmitidorEvento;
-use Alura\Arquitetura\Shared\Infra\Evento\LeagueEvent\Evento as LeagueEventEvento;
-use League\Event\Emitter;
+use Alura\Arquitetura\Shared\Infra\DI\ContainerCreator;
 use Alura\Arquitetura\Academico\Aplicacao\Aluno\MatricularAluno\{MatriculaAluno, MatriculaAlunoDto};
-use Alura\Arquitetura\Academico\Infra\Aluno\{CriptografadorDeSenhaMd5, CriptografadorDeSenhaPadrao, CriptografadorDeSenhaArgon2};
-use Alura\Arquitetura\Academico\Infra\Aluno\RepositorioAlunoPdo;
-
-$conexao = new PDO('sqlite::memory:');
-$conexao->exec('CREATE TABLE alunos (cpf TEXT PRIMARY KEY, nome TEXT, email TEXT);');
 
 $parametros = $argv;
 array_shift($parametros);
 
-$emitter = new Emitter();
-$emitter->addListener(AlunoMatriculado::class, function (LeagueEventEvento $leagueEventEvento) {
-    /** @var AlunoMatriculado $eventoDominio */
-    $eventoDominio = $leagueEventEvento->eventoDominio();
-    fputs(STDERR, $eventoDominio->cpfAluno() . ' matriculado em ' . $eventoDominio->momento()->format('d/m/Y'));
-});
-
 $dados = new MatriculaAlunoDto(...$parametros);
-$service = new MatriculaAluno(new RepositorioAlunoPdo($conexao), new CriptografadorDeSenhaArgon2(), new LeagueEventEmitidorEvento($emitter));
-$service->cadastra($dados);
+$service = ContainerCreator::criarContainer()->get(MatriculaAluno::class);
 
-$conexao->query('SELECT * FROM alunos')->fetchAll();
+$service->cadastra($dados);
---
+++ src/Shared/Infra/DI/ContainerCreator.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Alura\Arquitetura\Shared\Infra\DI;
+
+use Alura\Arquitetura\Academico\Dominio\Aluno\AlunoMatriculado;
+use Alura\Arquitetura\Academico\Dominio\Aluno\CriptografadorDeSenha;
+use Alura\Arquitetura\Academico\Dominio\Aluno\RepositorioAluno;
+use Alura\Arquitetura\Academico\Infra\Aluno\CriptografadorDeSenhaArgon2;
+use Alura\Arquitetura\Academico\Infra\Aluno\RepositorioAlunoPdo;
+use Alura\Arquitetura\Shared\Dominio\Evento\EmitidorEvento;
+use Alura\Arquitetura\Shared\Infra\Evento\LeagueEvent\EmitidorEvento as LeagueEventEmitidorEvento;
+use Alura\Arquitetura\Shared\Infra\Evento\LeagueEvent\Evento as LeagueEventEvento;
+use DI\ContainerBuilder;
+use League\Event\Emitter;
+use PDO;
+use Psr\Container\ContainerInterface;
+use function DI\autowire;
+use function DI\factory;
+
+final class ContainerCreator
+{
+    public static function criarContainer(): ContainerInterface
+    {
+        $builder = new ContainerBuilder();
+        $builder->addDefinitions(self::definitions());
+
+        return $builder->build();
+    }
+
+    private static function definitions()
+    {
+        return [
+            RepositorioAluno::class => autowire(RepositorioAlunoPdo::class),
+            EmitidorEvento::class => autowire(LeagueEventEmitidorEvento::class),
+            CriptografadorDeSenha::class => autowire(CriptografadorDeSenhaArgon2::class),
+            PDO::class => factory(function () {
+                $pdo = new PDO('sqlite::memory:');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $pdo->exec('
+                    CREATE TABLE indicacoes (cpf_indicante TEXT, cpf_indicado TEXT, data_indicacao TEXT, PRIMARY KEY (cpf_indicante, cpf_indicado));
+                    CREATE TABLE alunos (cpf TEXT PRIMARY KEY, nome TEXT, email TEXT);
+                    CREATE TABLE telefones (ddd TEXT, numero TEXT, cpf_aluno TEXT, PRIMARY KEY (ddd, numero));
+                ');
+
+                return $pdo;
+            }),
+            Emitter::class => factory(fn () => self::mapeamentoEventos())
+        ];
+    }
+
+    private static function mapeamentoEventos(): Emitter
+    {
+        $emitter = new Emitter();
+
+        $emitter->addListener(AlunoMatriculado::class, function (LeagueEventEvento $leagueEventEvento) {
+            /** @var AlunoMatriculado $eventoDominio */
+            $eventoDominio = $leagueEventEvento->eventoDominio();
+
+            fprintf(
+                STDERR,
+                "Aluno com CPF %s foi matriculado em %s\n",
+                $eventoDominio->cpfAluno(),
+                $eventoDominio->momento()->format('d/m/Y')
+            );
+        });
+
+        return $emitter;
+    }
+}
